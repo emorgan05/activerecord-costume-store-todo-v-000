12c141118d685ea3f3797438348a5bcc83f95681
Emily Morgan <emorgan05@gmail.com>
2017-10-26 19:45:24 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,7 +1,10 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
-
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.text :image_url
     end
   end
 end
